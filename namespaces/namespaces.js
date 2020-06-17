21819916237c900d4dd1a912116e75ebe434e176
lucas.castro@acct.global <lucas.sc98@hotmail.com>
2020-06-17 20:02:37 -0300

    Section 6 - Modules and namespaces

---
+++ namespaces/namespaces.js
@@ -0,0 +1,20 @@
+"use strict";
+///<reference path="geometriaCirc.ts"/>
+///<reference path="geometriaRet.ts"/>
+// namespace Geometria {
+//     export namespace Area {
+//         const PI = 3.14
+//         export function circunferencia(raio: number): number {
+//             return PI * Math.pow(raio, 2)
+//         }
+//         export function retangulo(base: number, altura: number): number {
+//             return base * altura
+//         }
+//     }
+// }
+// tsc -w --outfile namespace.js geometriaCirc.ts geometriaRet.ts namespace.ts
+// const PI = 2.99
+console.log(Geometria.Area.circunferencia(10));
+console.log(Geometria.Area.retangulo(10, 20));
+// console.log(PI)
+//# sourceMappingURL=namespaces.js.map
